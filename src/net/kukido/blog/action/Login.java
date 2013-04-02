76e4ad4d747abf5860063db8b45c2a34df9b2700
craser <craser@ca067221-1ae6-4d9b-88d4-2c81356bceaf>
2013-04-02 00:02:55 +0000

    Improved logging.
    git-svn-id: file:///Users/craser/Development/svnroot/KukidoBlog@294 ca067221-1ae6-4d9b-88d4-2c81356bceaf

--- src/net/kukido/blog/action/Login.java
+++ src/net/kukido/blog/action/Login.java
@@ -10,44 +10,49 @@ import net.kukido.blog.dataaccess.*;
 import net.kukido.blog.datamodel.*;
 import net.kukido.blog.security.*;
 import net.kukido.blog.forms.*;
+import net.kukido.blog.log.Logging;
 
 import java.io.*;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import org.apache.log4j.Logger;
 import org.apache.struts.action.*;
 
 /**
- *
- * @author  craser
+ * 
+ * @author craser
  * @version
  */
-public class Login extends Action
+public class Login extends Action 
 {
-    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest req, HttpServletResponse res)
-	throws ServletException, IOException
-    {
-        try
-        {
-            LoginForm loginForm = (LoginForm)form;
-	    String username = loginForm.getUsername();
-	    String password = loginForm.getPassword();
-	    User user = new UserDao().findByUserNamePassword(username, password);
-            req.getSession().setAttribute("user", user);
-            
-            if (loginForm.getRemember())
-            {
-		LoginCookie lc = new LoginCookie(username, password);
-		lc.setMaxAge(60 * 60 * 24 * 7); // Persist for one week.
-		res.addCookie(lc);
-	    }
-	    
-            return mapping.findForward("success");
-        }
-        catch (DataAccessException e)
-        {
-            throw new ServletException(e);
-        }
-    }
-}
+	private final Logger log = Logging.getLogger(getClass());
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest req, HttpServletResponse res)
+		throws ServletException, IOException 
+	{
+		LoginForm loginForm = (LoginForm) form;
+		String username = loginForm.getUsername();
+		String password = loginForm.getPassword();
+		try {
+			User user = new UserDao().findByUserNamePassword(username, password);
+			log.info("User " + username + " logged in.");
+			req.getSession().setAttribute("user", user);
 
+			if (loginForm.getRemember()) {
+				LoginCookie lc = new LoginCookie(username, password);
+				lc.setMaxAge(60 * 60 * 24 * 7); // Persist for one week.
+				log.debug("Setting login cookie: " + lc);
+				res.addCookie(lc);
+			}
+
+			log.debug("Login successful.");
+			return mapping.findForward("success");
+		} 
+		catch (DataAccessException e) {
+			log.info("Login for user " + username + " unsucessful.", e);
+			throw new ServletException(e);
+		}
+	}
+}
