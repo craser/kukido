e83e1c45bd8ee8cfa3fdb3239c9adf12087f88d9
craser <craser@ca067221-1ae6-4d9b-88d4-2c81356bceaf>
2013-08-18 18:02:05 +0000

    Re-enabled access to original attachments that are NOT jpgs, and enhanced UrlMapFilter config to let patterns be case-insensitive.
    git-svn-id: file:///Users/craser/Development/svnroot/branches/KukidoBlog%20-%20Attachment%20Deletion%20Integrity%20Fix@321 ca067221-1ae6-4d9b-88d4-2c81356bceaf

--- src/net/kukido/servlet/filter/UrlMapping.java
+++ src/net/kukido/servlet/filter/UrlMapping.java
@@ -2,30 +2,31 @@ package net.kukido.servlet.filter;
 
 import java.util.regex.*;
 
-public class UrlMapping
-{
-    private Pattern pattern;
-    private String template;
+public class UrlMapping {
+	private Pattern pattern;
+	private String template;
 
-    public UrlMapping(String pattern, String template)
-    {
-	this.pattern = Pattern.compile(pattern);
-	this.template = template;
-    }
+	public UrlMapping(String pattern, String template) {
+		this(pattern, template, true);
+	}
+	
+	public UrlMapping(String pattern, String template, boolean caseSensitive) {
+		this.pattern = caseSensitive 
+				? Pattern.compile(pattern)
+				: Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+		this.template = template;
+	}
 
-    public boolean matches(String uri)
-    {
-	return pattern.matcher(uri).matches();
-    }
+	public boolean matches(String uri) {
+		return pattern.matcher(uri).matches();
+	}
 
-    public String getResource(String uri)
-    {
-	return pattern.matcher(uri).replaceFirst(template);
-    }
-    
-    public String toString()
-    {
-        return "UrlMapping[\"" + pattern + "\" --> \"" + template + "\"]";
-    }
-	    
+	public String getResource(String uri) {
+		return pattern.matcher(uri).replaceFirst(template);
+	}
+
+	public String toString() {
+		return "UrlMapping[\"" + pattern + "\" --> \"" + template + "\"]";
+	}
+
 }
