a3c3067a1dc101cf9db8b04852c000d7d3872ef9
craser <craser@ca067221-1ae6-4d9b-88d4-2c81356bceaf>
2013-10-11 05:17:21 +0000

    Removed problematic colors from list of colors for trail maps page.
    git-svn-id: file:///Users/craser/Development/svnroot/KukidoBlog@337 ca067221-1ae6-4d9b-88d4-2c81356bceaf

--- WebContent/javascript/colors.js
+++ WebContent/javascript/colors.js
@@ -1,24 +1,19 @@
 colors = [
     "#9F5F9F", // Blue Violet
     "#FF7F00", // Coral
-    "#42426F", // Corn Flower
     "#9932CD", // * Dark Orchid
     "#871F78", // * Dark Purple
     "#6B238E", // * Dark Slate Blue
     "#7093DB", // Dark Turquoise
     "#D19275", // Feldspar
     "#8E2323", // * Firebrick
-    "#E47833", // Mandarian Orange
     "#8E236B", // * Maroon
     "#23238E", // * Navy Blue
     "#4D4DFF", // * Neon Blue
-    "#FF6EC7", // Neon Pink
     "#00009C", // New Midnight Blue
     "#FF7F00", // Orange
     "#FF2400", // Orange Red
     "#DB70DB", // * Orchid
-    "#5959AB", // Rich Blue
-    "#6F4242", // Salmon
     "#8C1717", // * Scarlet
     "#8E6B23", // Sienna
     "#3299CC", // Sky Blue
@@ -27,7 +22,6 @@ colors = [
     "#236B8E", // Steel Blue
     "#38B0DE", // Summer Sky
     "#D8BFD8", // Thistle
-    "#4F2F4F", // Violet
     "#CC3299", // Violet Red
 ];
 
