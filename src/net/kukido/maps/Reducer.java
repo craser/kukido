372a34e5c8b4b9e82bd1713ab3c6e938fc6491c2
Christopher Raser <chris@dreadedmonkeygod.net>
2015-09-14 10:27:08 -0700

    Fixed encoding issue in JavaDocs.

--- src/net/kukido/maps/Reducer.java
+++ src/net/kukido/maps/Reducer.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Implementation of Ramer–Douglas–Peucker algorithm.
+ * Implementation of Ramer-Douglas-Peucker algorithm.
  * @author craser
  *
  */
