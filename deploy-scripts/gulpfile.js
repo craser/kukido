20b2f6c073fb7cfbf6b7b522d654f2882f0d8e66
Chris <chris@dreadedmonkeygod.net>
2019-03-13 20:31:07 -0700

    Added gulp

---
+++ deploy-scripts/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var nconf = require('nconf');
+var browserify = require('gulp-browserify');
+var Q = require('q');
+
+var argv = require('yargs').argv;
+
+var workingPath = argv.basedir;
+var jsSrc = workingPath + '/../WebContent/javascript';
+var jsDst = workingPath + '/../WebContent/js';
+
+
+nconf.use('file', { file: workingPath + '/config.json' });
+
+gulp.task('default', () => {
+	var pages = nconf.get('pages');
+	console.log('pages: ' + pages);
+	var promises = [];
+	pages.forEach((p) => {
+		var srcFile = jsSrc + '/' + p.name + '.js';
+		console.log({ name: p.name, src: srcFile, dst: jsDst });
+		var promise = gulp.src(srcFile)
+			.pipe(browserify({
+				insertGlobals: true,
+				debug: true
+			}))
+			.pipe(gulp.dest(jsDst));
+		promises.push(promise);
+	});
+
+	return Q.all(promises);
+});
