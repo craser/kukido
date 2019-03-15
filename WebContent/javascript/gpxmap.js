135178c2ba4ef379cde470f3a57778bdfcf5aaf1
Chris <chris@dreadedmonkeygod.net>
2019-03-15 14:23:25 -0700

    Moved imported JS files.

--- WebContent/javascript/gpxmap.js
+++ WebContent/javascript/gpxmap.js
@@ -1,4 +1,4 @@
-var MapUI = require('./mapui');
+var MapUI = require('./maps/mapui');
 
 $(window).load(function() {
   window.mapui = new MapUI($('#map').data('map'),
--- WebContent/javascript/trailmaps.js
+++ WebContent/javascript/trailmaps.js
@@ -1,4 +1,4 @@
-var TrailheadMapUI = require('./trailheadmapui');
+var TrailheadMapUI = require('./maps/trailheadmapui');
 
 // Assign the actual Google Map obj. to the global var.
 $(window).load(function() {
